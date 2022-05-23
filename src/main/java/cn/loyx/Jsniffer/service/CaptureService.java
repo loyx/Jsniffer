e77e19b9ee8d905760efce6d9f66a2193568bdc1
loyx <isluoyixuan1@163.com>
2022-05-23 16:12:05 +0800

    :construction: feat(service): add startCapture function

--- src/main/java/cn/loyx/Jsniffer/service/CaptureService.java
+++ src/main/java/cn/loyx/Jsniffer/service/CaptureService.java
@@ -1,9 +1,16 @@
 package cn.loyx.Jsniffer.service;
 
+import cn.loyx.Jsniffer.capture.Extractor;
+import cn.loyx.Jsniffer.capture.Extractors;
+import org.jnetpcap.Pcap;
+import org.jnetpcap.packet.JPacket;
+import org.jnetpcap.packet.JPacketHandler;
+
 import javax.swing.table.DefaultTableModel;
 
 public class CaptureService {
     private final DefaultTableModel model;
+    private Pcap pcap;
 
     public CaptureService(DefaultTableModel model) {
         this.model = model;
@@ -11,8 +18,30 @@ public class CaptureService {
 
     public void startCapture(String devName){
         System.out.println("start capture:" + devName);
+        StringBuilder errBuf = new StringBuilder();
+        pcap = Pcap.openLive(devName, Pcap.DEFAULT_SNAPLEN, Pcap.DEFAULT_PROMISC, 60*1000, errBuf);
+        JPacketHandler<DefaultTableModel> handler = new JPacketHandler<DefaultTableModel>() {
+            @Override
+            public void nextPacket(JPacket packet, DefaultTableModel user) {
+                Extractor extractor = Extractors.createExtractor(packet);
+                Object[] rowData = {
+                        extractor.getNo(),
+                        extractor.getTimeStamp(),
+                        extractor.getSource(),
+                        extractor.getDestination(),
+                        extractor.getProtocol(),
+                        extractor.getLength(),
+                        extractor.getInfo()
+                };
+                model.addRow(rowData);
+            }
+        };
+        Thread thread = new Thread(() -> pcap.loop(Pcap.LOOP_INFINITE, handler, model));
+        thread.start();
+
     }
     public void stopCapture(){
         System.out.println("stop capture");
+        pcap.close();
     }
 }
