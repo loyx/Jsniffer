bb6d8947d6c659f98469cf1ae6aa0c9db7b13374
loyx <isluoyixuan1@163.com>
2022-05-26 21:43:33 +0800

    :bookmark: feat(Main): JSniffer 1.0 for win

--- src/main/java/cn/loyx/Jsniffer/Main.java
+++ src/main/java/cn/loyx/Jsniffer/Main.java
@@ -5,11 +5,35 @@ import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
+        loadLibrary();
         SwingUtilities.invokeLater(Main::createGUI);
+    }
+
+    private static void loadLibrary() {
+        try {
+            String location = "/lib/" + "jnetpcap.dll";
+            InputStream in = Main.class.getResourceAsStream(location);
+            File outFile = new File(System.getProperty("java.io.tmpdir") + File.separator + "JSniffer"+  location);
+            boolean mkdirs = outFile.getParentFile().mkdirs();
+            assert mkdirs;
+            FileOutputStream out  = new FileOutputStream(outFile);
+            int i;
+            byte[] buf = new byte[1024];
+            assert in != null;
+            while ((i = in.read(buf)) != -1){
+                out.write(buf, 0, i);
+            }
+            in.close();
+            out.close();
+            System.load(outFile.getPath());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void createGUI() {
